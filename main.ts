6ca86afeeda1afedc2922b464dc96751b60302ac
Sebimala <86785937+Sebimala@users.noreply.github.com>
2025-01-31 15:58:24 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,30 @@
-/**
- * radio road barrier
- */
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         servos.P0.setAngle(90)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     } else {
         servos.P0.setAngle(0)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
     }
 })
+/**
+ * radio road barrier
+ */
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P2, 10, NeoPixelMode.RGB)
 radio.setGroup(1)
+strip.clear()
