81ea9b07d97470b6faeb0bc3c243f06e509b0913
Sebimala <86785937+Sebimala@users.noreply.github.com>
2025-01-31 15:22:36 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+/**
+ * radio road barrier
+ */
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        servos.P0.setAngle(90)
+    } else {
+        servos.P0.setAngle(0)
+    }
 })
+radio.setGroup(1)
